7d09fe55946d7dbcefeaadff2cb906b52b5b400a
responsiblehemmher <hemmher12@gmail.com>
2024-06-13 15:52:26 +0100

    node first commit

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+/* const { authenticate } = require('passport')
+
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport) {
+    const authenticateUser = (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user === null ) {
+            return done(null, false, { message: 'No user with that email' })
+        }
+
+        try {
+            if(bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: 'Password incorrect' })
+            }
+        } catch (error) {
+            return done(error)
+        }
+    } 
+    passport.use(new LocalStrategy({ usernameField: 'email'}), 
+    authenticateUser)
+    passport.serializeUser((user, done) => { })
+    passport.deserializeUser((id, done) => { })
+}
+
+modules.exports = initialize; */
